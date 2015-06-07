88bb5bd48f1424c35023a32931517a77920d3267
jakelacombe <jake.lacombe2@gmail.com>
2015-06-07 11:52:09 -0500

    Add a Chart for 311 Report counts by year

--- app/controllers/neighborhood_controller.rb
+++ app/controllers/neighborhood_controller.rb
@@ -1,10 +1,22 @@
 class NeighborhoodController < ApplicationController
   def index
     @neighborhood = Neighborhood.find_by!(name: NeighborhoodSearch.search(params[:search]))
+    @neighborhood_data = Neighborhood311DataParser.new(
+      HTTParty.get(
+        URI::escape("https://data.kcmo.org/resource/7at3-sxhp.json?neighborhood=#{@neighborhood.name}")
+      )
+    )
   end
 
   def show
     @neighborhood = Neighborhood.find(params[:id])
+    @neighborhood_data = NeighborhoodDataParser.new(
+      HTTParty.get(
+        URI::escape("https://data.kcmo.org/resource/7at3-sxhp.json?neighborhood=#{@neighborhood.name}")
+      )
+    )
+
     render 'index'
   end
 end
+
