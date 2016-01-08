9bc1f03af4f08d6580c084fc010e40d7cf96b2ce
Jake LaCombe <jake.lacombe2@gmail.com>
2016-01-08 00:15:42 -0600

    Fix Bower Rails configuations

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -11,16 +11,9 @@ BowerRails.configure do |bower_rails|
   # Invokes rake bower:clean before precompilation. Defaults to false
   bower_rails.clean_before_precompile = true
 
-  # Excludes specific bower components from clean. Defaults to nil
-  bower_rails.exclude_from_clean = ['moment']
-
-  # Invokes rake bower:install:deployment instead of rake bower:install. Defaults to false
+  # Invokes rake bower:install:deployment instead rake bower:install. Defaults to false
   bower_rails.use_bower_install_deployment = true
-
-  # rake bower:install will search for gem dependencies and in each gem it will search for Bowerfile
-  # and then concatenate all Bowerfile for evaluation
-  bower_rails.use_gem_deps_for_bowerfile = true
-
-  # Passes the -F option to rake bower:install or rake bower:install:deployment. Defaults to false.
+  #
+  # Invokes rake bower:install and rake bower:install:deployment with -F (force) flag. Defaults to false
   bower_rails.force_install = true
 end
