027af95bc29642c271b2e58764773b46b10b5734
Jake LaCombe <jake.lacombe2@gmail.com>
2016-01-12 10:50:04 -0600

    Remove bower clean command due to bug

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -8,9 +8,6 @@ BowerRails.configure do |bower_rails|
   # Invokes rake bower:resolve before precompilation. Defaults to false
   bower_rails.resolve_before_precompile = true
 
-  # Invokes rake bower:clean before precompilation. Defaults to false
-  bower_rails.clean_before_precompile = true
-
   # Invokes rake bower:install:deployment instead rake bower:install. Defaults to false
   bower_rails.use_bower_install_deployment = true
   #
