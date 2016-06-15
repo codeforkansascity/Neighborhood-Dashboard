6e3802cf3bd38a995338ce722b5cdb00d3918e88
jakelacombe <jake.lacombe2@gmail.com>
2016-06-14 19:42:51 -0500

    Add Web Scraping Seeds to obtain Registered Vacant Lots

---
+++ app/models/registered_vacant_lot.rb
@@ -0,0 +1,2 @@
+class RegisteredVacantLot < ActiveRecord::Base
+end
---
+++ db/migrate/20160614022048_create_registered_vacant_lots.rb
@@ -0,0 +1,15 @@
+class CreateRegisteredVacantLots < ActiveRecord::Migration
+  def change
+    create_table :registered_vacant_lots do |t|
+      t.string :property_address
+      t.text :contact_person
+      t.text :contact_address
+      t.string :contact_phone
+      t.string :property_type
+      t.string :registration_type
+      t.datetime :last_verified
+
+      t.timestamps null: false
+    end
+  end
+end
