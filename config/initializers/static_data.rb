320b5e660df8448120553bdb1dc492d4d0818f07
Jake LaCombe <jake.lacombe2@gmail.com>
2016-08-12 13:36:07 -0500

    Produce the JSON needed for vacant lots

--- config/initializers/static_data.rb
+++ config/initializers/static_data.rb
@@ -1,7 +1,12 @@
 class StaticData
   @@parcel_data ||= JSON.parse(File.read(::Rails.root.join('data_sets', 'parcel_coordinates.json')))['features']
+  @@vacant_lot_data ||= JSON.parse(File.read(::Rails.root.join('data_sets', 'vacant_lots.json')))
 
   def self.PARCEL_DATA()
     @@parcel_data
+  end
+
+  def self.VACANT_LOT_DATA()
+    @@vacant_lot_data
   end
 end
