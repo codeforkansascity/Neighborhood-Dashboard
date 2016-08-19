11e32d503abef571275f000aed1ea94f12657019
Jake LaCombe <jake.lacombe2@gmail.com>
2016-08-19 10:20:12 -0500

    Round of unit testing

--- app/service/neighborhood_services/legally_abandoned_calculation/vacant_registries.rb
+++ app/service/neighborhood_services/legally_abandoned_calculation/vacant_registries.rb
@@ -7,7 +7,7 @@ class NeighborhoodServices::LegallyAbandonedCalculation::VacantRegistries
     vacant_registries_data = StaticData::VACANT_LOT_DATA().select{ |lot| lot['neighborhood'] == @neighborhood.name }
 
     vacant_registries_data.each_with_object({}) do |vacant_lot, points_hash|
-      display = "<b>Registration Type:</b> #{vacant_lot['registration_type']}<br/><b>Last Verified: #{vacant_lot['last_verified']}</b>"
+      display = "<b>Registration Type:</b> #{vacant_lot['registration_type']}<br/><b>Last Verified:</b> #{vacant_lot['last_verified']}"
 
       points_hash[vacant_lot['property_address'].downcase] = {
         points: 2,
---
+++ spec/services/neighborhood_services/legally_abandoned_calculation/three_eleven_data_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe NeighborhoodServices::LegallyAbandonedCalculation::ThreeElevenData do
+  let(:neighborhood) { double(name: 'Neighborhood')}
+  let(:dataset) { NeighborhoodServices::LegallyAbandonedCalculation::ThreeElevenData.new(neighborhood) }
+  let(:three_eleven_client_query) { double }
+  let(:three_eleven_data) {
+    [
+      {
+        'street_address' => 'Street Address 1',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 1',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 2',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 3',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 4',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 4',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      },
+      {
+        'street_address' => 'Street Address 4',
+        'request_type' => 'verified',
+        'address_with_geocode' => {
+          'coordinates' => [34.2342, 24.2343]
+        }
+      }
+    ]
+  }
+
+  before do
+    allow(KcmoDatasets::ThreeElevenCases).to receive(:new).and_return(three_eleven_client_query)
+    allow(three_eleven_client_query).to receive(:open_cases).and_return(three_eleven_client_query)
+    allow(three_eleven_client_query).to receive(:vacant_called_in_violations).and_return(three_eleven_client_query)
+    allow(three_eleven_client_query).to receive(:request_data).and_return(three_eleven_data)
+  end
+
+  describe '#calculated_data' do
+    let(:calculated_data) { dataset.calculated_data }
+
+    it 'creates a hash with the key as the lowercased address and a point for every violation' do
+      expect(calculated_data['street address 1'])
+    end
+  end
+end
