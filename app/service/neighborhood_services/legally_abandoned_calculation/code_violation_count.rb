8940801e7c2303ea21aa47f05fc62979ed2a8727
Jake LaCombe <jake.lacombe2@gmail.com>
2016-12-05 15:37:58 -0600

    Do violation count as opposed to violation length

--- app/service/neighborhood_services/legally_abandoned_calculation/code_violation_count.rb
+++ app/service/neighborhood_services/legally_abandoned_calculation/code_violation_count.rb
@@ -6,55 +6,34 @@ class NeighborhoodServices::LegallyAbandonedCalculation::CodeViolationCount
   def calculated_data
     addresses = {}
 
-    dataset = KcmoDatasets::PropertyViolations.new(@neighborhood)
-    code_violation_data = dataset
-                          .open_cases
-                          .request_data
-
+    code_violation_data = KcmoDatasets::PropertyViolations.grouped_address_counts(@neighborhood)
     code_violation_data.each do |address|
       mapping_address = address['address']
 
       if mapping_address.present?
+        current_violation_count = address['count_address'].to_i
 
-        # Fix this defect. The 2 should be a 1
-        points = if address['days_open'].to_i >= 365 * 3
+        points = if current_violation_count >= 3
                    2
-                 elsif address['days_open'].to_i >= 365
+                 elsif current_violation_count >= 2
                    1
                  else
                    0
                  end
 
+        message = "#{current_violation_count} Property Violations"
+
         if points > 0
-          current_address = address[mapping_address.downcase]
-
-          if current_address.present?
-            current_address[:points] = [points, currentAddress[:point]].max
-            current_address[:disclosure_attributes] << "#{address['violation_description'].titleize}: #{address['days_open'].to_i % 365} Years open"
-          else
-            source_link = "<a href='#{KcmoDatasets::PropertyViolations::SOURCE_URI}' target='_blank'>Source</a>"
-
-            addresses[mapping_address.downcase] = {
-              points: points,
-              longitude: address['mapping_location']['coordinates'][0].to_f,
-              latitude: address['mapping_location']['coordinates'][1].to_f,
-              disclosure_attributes: [
-                "<h3 class='info-window-header'>Current Violations:</h3>&nbsp;#{source_link}",
-                "Last Updated: #{last_updated_date(dataset.metadata)}",
-                "#{address['violation_description'].titleize}: #{(address['days_open'].to_i / 365).floor} Years open"
-              ]
-            }
-          end
+          addresses[mapping_address.downcase] = {
+            points: points,
+            longitude: address['mapping_location']['coordinates'][0].to_f,
+            latitude: address['mapping_location']['coordinates'][1].to_f,
+            disclosure_attributes: [message]
+          }
         end
       end
     end
 
     addresses
-  end
-
-  def last_updated_date(metadata)
-    DateTime.strptime(metadata['viewLastModified'].to_s, '%s').strftime('%m/%d/%Y')
-  rescue
-    'N/A'
   end
 end
