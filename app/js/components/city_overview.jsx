744b66dc0a50dd2e15a1ac5d60c873d48c07e7ad
Aaron McRuer <a.mcruer@live.com>
2017-07-20 10:17:59 -0500

    city_overview.jsx linting

--- app/js/components/city_overview.jsx
+++ app/js/components/city_overview.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { withGoogleMap, GoogleMap, Polygon, InfoWindow, Marker } from 'react-google-maps';
-import axios from 'axios';
 
 class CityOverview extends React.Component {
 
